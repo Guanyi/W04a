bda8414bf8af028c916710dd07e65db280ade95f
guanyi <guanyifang@hotmail.com>
2015-10-02 01:45:51 -0700

    initial push

---
+++ W04a/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="W04a.MvcApplication" Language="C#" %>
---
+++ W04a/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(W04a.Startup))]
+namespace W04a
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
